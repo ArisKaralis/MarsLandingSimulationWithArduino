129217b97bef90df35b25c41497cad233ba3e1c1
Aris <up958669@myport.ac.uk>
2023-03-28 11:27:00 +0100

    change

--- UP958669/UP958669.ino
+++ UP958669/UP958669.ino
@@ -19,12 +19,12 @@ MPU6050 mpu6050(Wire);
 #define BUZZER_PIN 2
 // Define constants for distance thresholds
 
-#define Parachute_Deploy 200        // cm
-#define Heat_Shield_Separation 170  // cm
-#define Radar_Lock 123              // cm
-#define Terrain_Relative_Navigation 62         // cm
-#define Backshell_Separation 33     // cm
-#define Rover_Separation 10         // cm
+#define Parachute_Deploy 200           // cm
+#define Heat_Shield_Separation 170     // cm
+#define Radar_Lock 123                 // cm
+#define Terrain_Relative_Navigation 62 // cm
+#define Backshell_Separation 33        // cm
+#define Rover_Separation 10            // cm
 
 // Define constants for Photoresistor(photocell)s
 #define PHOTO_RESISTOR_PIN A0
@@ -34,13 +34,15 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, Parachute_Deploy);
 // Define a variable to store the photoresistor value
 int photoresistorValue;
 
-void setColor(int red, int green, int blue) {
+void setColor(int red, int green, int blue)
+{
   analogWrite(BLUE_PIN, red);
   analogWrite(GREEN_PIN, green);
   analogWrite(RED_PIN, blue);
 }
 
-void setup() {
+void setup()
+{
   // Set trigger pin as output and echo pin as input
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
@@ -76,7 +78,8 @@ void setup() {
   mpu6050.calcGyroOffsets(true);
 }
 
-void loop() {
+void loop()
+{
 
   // Send a short pulse to trigger the sensor
   digitalWrite(TRIGGER_PIN, HIGH);
@@ -92,14 +95,14 @@ void loop() {
   // Read the photoresistor value
   photoresistorValue = analogRead(PHOTO_RESISTOR_PIN);
 
-  //Check the distance and turn on/off the LED lights accordingly
+  // Check the distance and turn on/off the LED lights accordingly
   Serial.print("Light Level: ");
   Serial.print(photoresistorValue);
   Serial.print(",Distance: ");
   Serial.println(distance);
 
   mpu6050.update();
-  //Print the X, Y and Z 
+  // Print the X, Y and Z
   Serial.print("Gyro X: ");
   Serial.print(mpu6050.getGyroX());
   Serial.print(" | Gyro Y: ");
@@ -107,19 +110,20 @@ void loop() {
   Serial.print(" | Gyro Z: ");
   Serial.println(mpu6050.getGyroZ());
 
-
-  //Colour initialisation
+  // Colour initialisation
   setColor(0, 0, 0);
 
-  if (distance > Parachute_Deploy) { //Parachute Deployment
+  if (distance > Parachute_Deploy)
+  { // Parachute Deployment
     // RGB BLUE
     setColor(0, 0, 255);
 
     Serial.println("Preparing for Parachute Deployment");
     // Turn off buzzer
     digitalWrite(BUZZER_PIN, LOW);
-
-  } else if (distance > Heat_Shield_Separation && distance <= Parachute_Deploy) { //Heat Shield Separation
+  }
+  else if (distance > Heat_Shield_Separation && distance <= Parachute_Deploy)
+  { // Heat Shield Separation
     // TRGB GREEN
     setColor(0, 255, 0);
 
@@ -127,8 +131,9 @@ void loop() {
     Serial.println("Preparing for Heat Shield Separation");
     // Turn off buzzer
     digitalWrite(BUZZER_PIN, LOW);
-
-  }  else if (distance > Radar_Lock && distance <= Heat_Shield_Separation) { //Radar Lock
+  }
+  else if (distance > Radar_Lock && distance <= Heat_Shield_Separation)
+  { // Radar Lock
     // RGB YELLOW
     setColor(255, 255, 0);
 
@@ -136,16 +141,18 @@ void loop() {
     Serial.println("Preparing for Radar Lock");
     // Turn off buzzer
     digitalWrite(BUZZER_PIN, LOW);
-
-    } else if (distance > Terrain_Relative_Navigation && distance <= Radar_Lock) { //Terrain Relative Navigation
+  }
+  else if (distance > Terrain_Relative_Navigation && distance <= Radar_Lock)
+  { // Terrain Relative Navigation
     // RGB WHITE
     setColor(255, 255, 255);
     Serial.println("Radar Lock Completed");
     Serial.println("Preparing for Terrain Relative Navigation");
     // Turn off buzzer
     digitalWrite(BUZZER_PIN, LOW);
-
-  } else if (distance > Backshell_Separation && distance <= Terrain_Relative_Navigation) { //Backshell Separation
+  }
+  else if (distance > Backshell_Separation && distance <= Terrain_Relative_Navigation)
+  { // Backshell Separation
     // RGB RED
     setColor(255, 0, 0);
 
@@ -153,35 +160,38 @@ void loop() {
     Serial.println("Preparing for Backshell Separation");
     // Turn off buzzer
     digitalWrite(BUZZER_PIN, LOW);
-
-  } else if (distance > Rover_Separation && distance < Backshell_Separation) { //Rover Separation
-    //RGB AQUA
-    setColor(0,255,255);
+  }
+  else if (distance > Rover_Separation && distance < Backshell_Separation)
+  { // Rover Separation
+    // RGB AQUA
+    setColor(0, 255, 255);
 
     Serial.println("Backshell Separation Completed");
     Serial.println("Preparing for Rover Separation");
     // Turn off buzzer
     digitalWrite(BUZZER_PIN, LOW);
-
-  } else if (distance <= Rover_Separation) {
+  }
+  else if (distance <= Rover_Separation)
+  {
     // Iterate through the colors
-    for (int r = 0; r <= 255; r += 51) {
-      for (int g = 0; g <= 255; g += 51) {
-        for (int b = 0; b <= 255; b += 51) {
+    for (int r = 0; r <= 255; r += 51)
+    {
+      for (int g = 0; g <= 255; g += 51)
+      {
+        for (int b = 0; b <= 255; b += 51)
+        {
           setColor(r, g, b);
           delay(50); // Adjust this value to change the speed of color transitions
         }
       }
     }
 
-    digitalWrite(BUZZER_PIN, HIGH);  // active the buzzer
-    delay(3000);                     // delay of 3000 millisecond = 3s
-    digitalWrite(BUZZER_PIN, LOW);   // deactivat the buzzer
+    digitalWrite(BUZZER_PIN, HIGH); // active the buzzer
+    delay(3000);                    // delay of 3000 millisecond = 3s
+    digitalWrite(BUZZER_PIN, LOW);  // deactivat the buzzer
 
     Serial.println("Rover Separation Completed");
-
   }
 
-  
   delay(1000);
 }
